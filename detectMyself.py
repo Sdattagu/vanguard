4c972fbee9d8be8f326c0e922f6cf7849e400651
Shounak Dattagupta <sdattagu@gmail.com>
2020-10-10 14:30:58 -0700

    player detection

---
+++ detectMyself.py
@@ -0,0 +1,37 @@
+import numpy as np
+import cv2
+
+def func_myself():
+    image = cv2.imread('test.png')
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+
+    lower_yellow = (28, 220, 254)
+    upper_yellow = (30, 250, 255)
+    lower_red = (0, 250, 200)
+    upper_red = (10, 255, 255)
+
+    mask = cv2.inRange(hsv_image, lower_yellow, upper_yellow)
+    result = cv2.bitwise_and(image, image, mask=mask)
+
+    # Find contours
+    cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Extract contours depending on OpenCV version
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+    # Iterate through contours and filter by the number of vertices
+    for c in cnts:
+        perimeter = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.04 * perimeter, True)
+        if len(approx) > 3:
+            (x, y), radius = cv2.minEnclosingCircle(c)
+            center = (int(x), int(y))
+            radius = int(radius)
+            print('Contour: centre {},{}, radius {}'.format(x, y, radius))
+            cv2.drawContours(result, [c], -1, (36, 255, 12), -1)
+
+    cv2.imshow('mask', mask)
+    cv2.imshow('original', result)
+    cv2.imwrite('mask.png', mask)
+    cv2.imwrite('original.png', result)
+    cv2.waitKey()
--- main.py
+++ main.py
@@ -3,56 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import time
-import character.movement.teleport as tele
-import character.movement.jump as jump
-import character.movement.jump_db as dbJump
-import character.movement.upperRowAttack as ura
-from controllers.windowCtrl import WindowCtrl as wctrl
-
-import character.classScripts.kanna.blueMushroomForest as blueMushForestScript
-import character.classScripts.kanna.desertOfSerenity as desertOfSerenScript
-import character.classScripts.kanna.rockyRoad as rockyRoadScript
-import character.classScripts.kanna.refugeSouth1 as refugeSouth1
-import character.classScripts.kanna.aboveTheCave as aboveTheCave
-import character.classScripts.nightLord.hiddenUndergroundTrain as hiddenUndergroundTrain
-import controllers.keyboardCtrlTest as kbd_test
-
-import controllers.keyboardCtrl as kbd
-import bindings.bottomLetterRow as blr
-import bindings.bottomFnRow as bfr
-import bindings.arrowKeys as ak
-
-q = 0x10
-w = 0x11
-e = 0x12
-r = 0x13
-t = 0x14
-y = 0x15
-
-a = 0x1E
-s = 0x1F
-d = 0x20
-f = 0x21
-g = 0x22
-
-z = 0x2C
-x = 0x2D
-c = 0x2E
-v = 0x2F
-
-rightArrow = 0xCD
-leftArrow = 0xCB
-downArrow = 0xD0
-
 # Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    w = wctrl()
-    w.find_window_wildcard(".*MapleStory.*")
-    w.set_foreground()
-    w.set_active()
-    time.sleep(1)
-    ##### Tests here ######
-    for iteration in range(1):
-        print("Starting iteration: ", iteration+1)
-        hiddenUndergroundTrain.main()
+    img = cv2.imread('C:\\Users\\Sho\\Pictures\\testopencv.jpg')
+    height, width, channels = img.shape
